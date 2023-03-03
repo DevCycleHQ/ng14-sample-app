e59f2a06897945ae8975dce23f21d6996e1e5aec
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-03-03 14:46:40 -0500

    initial commit
    this commit contains a sample app with the jssdk, it
    handles real time updates in a rudimentary way and
    replicates the warnings that the client pointed out
    when running `yarn start`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initialize } from '@devcycle/devcycle-js-sdk'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng14';
+  dvcClient = initialize('client-ed2daf14-82d5-47f6-8a73-a5c298546efa', { user_id: '123' });
+
+  ngOnInit() {
+    this.dvcClient.onClientInitialized().then(() => {
+      const featureToggle = this.dvcClient.variable('fadsfsad', false)
+      if (featureToggle.value) {
+          this.title = "got it"
+          console.log("got it")
+      } else {
+          this.title = "don't got it"
+          console.log("don't got it")
+      }
+      featureToggle.onUpdate(() => {
+        if (featureToggle.value) {
+          this.title = "got it"
+        } else {
+          this.title = "don't got it"
+        }
+      })
+    })
+  }
 }
